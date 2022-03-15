d1b2adcc5a8de4ff24f8874b09464dec29cda185
40923120 <40923120@gm.nfu.edu.tw>
2022-03-15 18:01:49 +0800

    test-2

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n', 'tags': '', 'url': 'About.html'}, {'title': '小組名單', 'text': '組長 \n', 'tags': '', 'url': '小組名單.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n', 'tags': '', 'url': 'About.html'}, {'title': '小組名單', 'text': '組長:40923120 李捷綸 \n 網站   倉儲 \n 組員:40923117李文揚 \n 網站 倉儲 \n 40923118李明軒 \n 網站 倉儲 \n 40923147張程翔 \n 網站 倉儲', 'tags': '', 'url': '小組名單.html'}]};
